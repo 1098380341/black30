a7580da9224ea744814b1d1b38378b9f23fa15c3
Your Name <you@exam    ple.com>
2019-04-30 11:30:52 +0800

    整合路由以及首页

--- src/main.js
+++ src/main.js
@@ -4,8 +4,28 @@ import App from './App.vue'
 // 导入样式
 import './assets/statics/site/css/style.css'
 
+// 导入首页
+import index from './components/index.vue'
+
+// 导入路由
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+const routes=[
+  {
+    path:'/index',
+    component:index
+  }
+]
+
+const router=new VueRouter({
+  routes
+})
+
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
