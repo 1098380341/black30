3cf0b4ae7c1732fba6d1e472f5f72097be4952ba
Your Name <you@exam    ple.com>
2019-04-30 10:54:08 +0800

    项目初始化以及头部尾部整合

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+// 导入样式
+import './assets/statics/site/css/style.css'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
