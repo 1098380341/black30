c8bd7e281892335d474b89ff1d11732f8de4b3b5
Your Name <you@exam    ple.com>
2019-05-03 17:20:39 +0800

    主页下半部分以及详情页的整合

--- src/main.js
+++ src/main.js
@@ -6,15 +6,27 @@ import './assets/statics/site/css/style.css'
 
 // 导入首页
 import index from './components/index.vue'
+// 导入详情页
+import detail from './components/detail.vue'
+
+// 导入饿了吗插件
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
 
 // 导入路由
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
-
+// 写规则  linkto后经过规则 在跳转响应的网页
 const routes=[
   {
     path:'/index',
     component:index
+  },
+  {
+    path:'/detail/:id',
+    component:detail
   }
 ]
 
