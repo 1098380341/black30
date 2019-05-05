f5e231eb4a2a2f27fef11c5505c13fc97d0f1548
chenguanxi <1098380341@qq.com>
2019-05-05 14:57:54 +0800

    轮播图和评论的整合

--- src/components/detail.vue
+++ src/components/detail.vue
@@ -13,8 +13,12 @@
         <div class="wrap-box">
           <div class="left-925">
             <div class="goods-box clearfix">
-              <div class="pic-box" v-for="(item, index) in imglist" :key="index">
-                  <img :src="item.thumb_path" alt="">
+              <div class="pic-box">
+                <el-carousel height="330px">
+                  <el-carousel-item v-for="item in imglist " :key="item.id">
+                      <img :src="item.thumb_path" alt="" class="slide-img">
+                  </el-carousel-item>
+                </el-carousel>
               </div>
               <div class="goods-spec">
                 <h1>{{goodsinfo.title}}</h1>
@@ -42,35 +46,7 @@
                     <dt>购买数量</dt>
                     <dd>
                       <div class="stock-box">
-                        <div class="el-input-number el-input-number--small">
-                          <span role="button" class="el-input-number__decrease is-disabled">
-                            <i class="el-icon-minus"></i>
-                          </span>
-                          <span role="button" class="el-input-number__increase">
-                            <i class="el-icon-plus"></i>
-                          </span>
-                          <div class="el-input el-input--small">
-                            <!---->
-                            <input
-                              autocomplete="off"
-                              size="small"
-                              type="text"
-                              rows="2"
-                              max="60"
-                              min="1"
-                              validateevent="true"
-                              class="el-input__inner"
-                              role="spinbutton"
-                              aria-valuemax="60"
-                              aria-valuemin="1"
-                              aria-valuenow="1"
-                              aria-disabled="false"
-                            >
-                            <!---->
-                            <!---->
-                            <!---->
-                          </div>
-                        </div>
+                        <el-input-number v-model="num" :min="1" :max="goodsinfo.stock_quantity" label="描述文字"></el-input-number>
                       </div>
                       <span class="stock-txt">
                         库存
@@ -97,16 +73,16 @@
               >
                 <ul>
                   <li>
-                    <a href="javascript:;" class="selected">商品介绍</a>
+                    <a href="javascript:;" @click="showdes=true" :class="{selected:showdes}">商品介绍</a>
                   </li>
                   <li>
-                    <a href="javascript:;">商品评论</a>
+                    <a href="javascript:;" @click="showdes=false" :class="{selected:!showdes}">商品评论</a>
                   </li>
                 </ul>
               </div>
-              <div class="tab-content entry" style="display: block;"
-               v-html="goodsinfo.content">内容</div>
-              <div class="tab-content" style="display: block;">
+              <div class="tab-content entry" style="display: block;"  v-show="showdes" v-html="goodsinfo.content">内容</div>
+
+              <div class="tab-content" style="display: block;"  v-show="!showdes" >
                 <div class="comment-box">
                   <div id="commentForm" name="commentForm" class="form-box">
                     <div class="avatar-box">
@@ -173,6 +149,7 @@
                   </div>
                 </div>
               </div>
+
             </div>
           </div>
           <div class="left-220">
@@ -183,9 +160,7 @@
                   <li v-for="(item, index) in hotgoodslist" :key="index">
                     <div class="img-box">
                       <a href="#/site/goodsinfo/90" class>
-                        <img
-                          :src="item.img_url"
-                        >
+                        <img :src="item.img_url">
                       </a>
                     </div>
                     <div class="txt-box">
@@ -215,7 +190,11 @@ export default {
       //  右侧详情
       hotgoodslist: [],
       // 底下图片详情
-      imglist: []
+      imglist: [],
+      // 计数器的数值
+      num:1,
+      // 详情页和评论切换
+      showdes:false,
     };
   },
   created() {
@@ -223,30 +202,32 @@ export default {
     // 用了router 自动生成 $route  id在params里面 直接点出来
     // 获取id后 发axios请求
     this.$axios
-      .get(
-        `/site/goods/getgoodsinfo/${
-          this.$route.params.id
-        }`
-      )
+      .get(`/site/goods/getgoodsinfo/${this.$route.params.id}`)
       .then(res => {
         console.log(res);
-       this.goodsinfo=res.data.message.goodsinfo;
-        this.hotgoodslist=res.data.message.hotgoodslist;
-        this.imglist=res.data.message.imglist;
+        this.goodsinfo = res.data.message.goodsinfo;
+        this.hotgoodslist = res.data.message.hotgoodslist;
+        this.imglist = res.data.message.imglist;
       });
-  },
+  }
   // filters:{
   //     formatTime(value){
   //         return moment(value).format("YYYY年MM月DD日");
   //     }
   // }
-
 };
 </script>
 
 <style>
-.tab-content img{
+.tab-content img {
   display: block;
   width: 100%;
+}
+
+.pic-box{
+  width: 395px;
+}
+.pic-box .slide-img{
+  height: 100%;
 }
 </style>
